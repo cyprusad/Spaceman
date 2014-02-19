1adf3b19baa33d5d917eaf3ee169157b9c319b21
KMassicotte <Keelan1013@gmail.com>
2014-02-19 02:41:48 -0500

    Basic caveman animations.
    Basic walk and hit animations added for the caveman.
    
    Known Issues:
    the animation caveman_hit_west is currently backwards.

---
+++ Assets/Caveman/Scripts/CavemanController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CavemanController : MonoBehaviour {
+
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var vertical = Input.GetAxis("Vertical");
+		var horizontal = Input.GetAxis("Horizontal");
+
+		animator.SetBool("Attack", false);
+
+		if (vertical > 0)
+		{
+			animator.SetInteger("Direction", 0);
+		}
+		else if (vertical < 0)
+		{
+			animator.SetInteger("Direction", 2);
+		}
+		else if (horizontal > 0)
+		{
+			animator.SetInteger("Direction", 1);
+		}
+		else if (horizontal < 0)
+		{
+			animator.SetInteger("Direction", 3);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)){
+			print ("space key was pressed");
+			animator.SetBool("Attack", true);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## Spaceman ##
+
+**Firefly meets Pikmin!**<br>
+An RPG, resource gathering, space exploration game. 
+
+#### Built in Unity ####
+
+We believe Unity gives us both the flexibility and the power to accomplish our goals and bring our vision to life.
+
+So buckle up and get ready to explore our personal space!
